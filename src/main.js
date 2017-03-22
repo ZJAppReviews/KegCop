6a6be5e16564ed52933193af3a2ce01086e9b5e2
ipatch <chris.r.jones.1983@gmail.com>
2017-03-21 20:15:57 -0500

    try and get initial styling working for app by setting a background-color

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 console.log('Hello World!');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+
+import './styles/styles.css';
+
+render(
+  document.getElementById('app')
+);
