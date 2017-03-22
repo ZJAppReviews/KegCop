95f792819ff41e8887a4668c672d9444583d26f2
ipatch <chris.r.jones.1983@gmail.com>
2017-03-21 20:52:33 -0500

    working on getting hot reloading working

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,72 @@
-const path = require('path');
+const { resolve } = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  context: path.join(__dirname, 'src'),
   entry: [
-    './main.js',
+    'react-hot-loader/patch',
+    // activate HMR for React
+
+    'webpack-dev-server/client?http://localhost:8080',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+
+    './main.js'
+    // the entry point of our app
   ],
   output: {
-    path: path.join(__dirname, 'www'),
     filename: 'bundle.js',
+    // the output bundle
+
+    path: resolve(__dirname, 'dist'),
+
+    publicPath: '/'
+    // necessary for HMR to know where to load the hot update chunks
   },
+
+  context: resolve(__dirname, 'src'),
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    hot: true,
+    // enable HMR on the server
+
+    contentBase: resolve(__dirname, 'dist'),
+    // match the output path
+
+    publicPath: '/'
+    // match the output `publicPath`
+  },
+
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
         use: [
           'babel-loader',
         ],
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
+        use: [
+          'style-loader',
+          'css-loader?modules',
+          'postcss-loader',
+        ],
       },
     ],
   },
-  resolve: {
-    modules: [
-      path.join(__dirname, 'node_modules'),
-    ],
-  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+  ],
 };
