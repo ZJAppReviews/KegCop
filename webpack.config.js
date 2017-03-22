e404d51628a0fa4e61ad49d52f983344084bfa62
ipatch <chris.r.jones.1983@gmail.com>
2017-03-21 20:00:15 -0500

    add css-loader / style-loader (loaders) to project

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,14 @@ module.exports = {
           'babel-loader',
         ],
       },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      },
     ],
   },
   resolve: {
