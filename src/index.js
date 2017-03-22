b517774744171c251c1da782c5bd901e19f1ba46
ipatch <chris.r.jones.1983@gmail.com>
2017-03-21 22:23:25 -0500

    rename main.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-console.log('Hello!');
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
 
 import './styles/styles.css';
 
-render(
-  document.getElementById('app')
-);
+console.log("Hello, World");
