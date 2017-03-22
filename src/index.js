b92ec12b43234c801e753274ff0bf631960ea5eb
ipatch <chris.r.jones.1983@gmail.com>
2017-03-21 22:58:13 -0500

    minor changes

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ import { render } from 'react-dom';
 import './styles/styles.css';
 
 console.log("Hello, World");
+
+ReactDOM.render(
+  <h1>Hello KegCop.</h1>,
+  document.getElementById('app')
+);
