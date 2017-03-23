8e95b07a640465e9d8d8ff19c543314caecf3a56
ipatch <chris.r.jones.1983@gmail.com>
2017-03-22 22:43:12 -0500

    breaking stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import styles from './App.css';
+import NavBar from './common/Navbar';
+import NavLink from './common/NavLink';
+
+
+
+
 
 const App = () => (
   <div className={styles.app}>
+    <NavBar />
     <h2>Hello, KegCop.</h2>
   </div>
 );
