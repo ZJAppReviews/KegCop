093a25e293bf5f2639bbe4229046203d8af819d7
ipatch <chris.r.jones.1983@gmail.com>
2017-03-27 21:55:22 -0500

    clean up of `webpack.config.prod.js`

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,17 +3,7 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: [
-    // 'react-hot-loader/patch',
-    // activate HMR for React
-
-    // 'webpack-dev-server/client?http://localhost:8080',
-    // bundle the client for webpack-dev-server
-    // and connect to the provided endpoint
-
-    // 'webpack/hot/only-dev-server',
-    // bundle the client for hot reloading
-    // only- means to only hot reload for successful updates
-
+    
     './index.js'
     // the entry point of our app
   ],
@@ -21,22 +11,17 @@ module.exports = {
     filename: 'bundle.js',
     // the output bundle
 
-    path: resolve(__dirname, 'docs'), // changed 'dist' to 'www'
+    path: resolve(__dirname, 'docs'),
 
     publicPath: '/'
-    // necessary for HMR to know where to load the hot update chunks
+
   },
 
   context: resolve(__dirname, 'src'),
 
-  // devtool: 'inline-source-map',
-  // devtool: "source-map"
-
   devServer: {
-    // hot: true,
-    // enable HMR on the server
 
-    contentBase: resolve(__dirname, 'docs'), // changed 'dist' to 'www'
+    contentBase: resolve(__dirname, 'docs'),
     // match the output path
 
     publicPath: '/'
@@ -64,19 +49,9 @@ module.exports = {
   },
 
   plugins: [
-    // new webpack.HotModuleReplacementPlugin(),
-    // enable HMR globally
-
-    // new webpack.NamedModulesPlugin(),
-    // prints more readable module names in the browser console on HMR updates
-
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
     }),
-
-    // new webpack.optimize.UglifyJsPlugin({
-    //   sourceMap: options.devtool && (options.devtool.indexOf("sourcemap") >= 0 || options.devtool.indexOf("source-map") >= 0)
-    //   })
   ],
 };
