7aee338370e92a95e4579e8db76be2afe083c7d2
ipatch <chris.r.jones.1983@gmail.com>
2017-03-28 11:56:47 -0500

    working on getting navbar to render on screen.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import styles from './App.css';
-// import NavBar from './common/Navbar';
+import NavBar from './common/Navbar';
 // import NavLink from './common/NavLink';
+
+var navbar = {};
+navbar.brand = {linkTo: "/", text: "chrisrjones.com"};
+navbar.links = [
+    {linkTo: "/about", text: "About Me"},
+    {linkTo: "/contact", text: "Contact" },
+    {dropdown: true, text: "Contribute",  links: [
+        {linkTo: "signup", text: "Sign Up" },
+        {linkTo: "login", text: "Login" }
+    ]}
+];
 
 const App = () => (
   <div className={styles.app}>
-
-    <h2>Hello, KegCop.</h2>
+    <NavBar {...navbar}/>
+    <h2>Hello, KegCop!</h2>
+    {this.props.children}
   </div>
 );
+
+App.propTypes = {
+	children: PropTypes.object.isRequired
+};
 
 export default App;
