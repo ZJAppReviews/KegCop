10e69401f2b419ef6d93561fc775483f46dd4531
ipatch <chris.r.jones.1983@gmail.com>
2017-03-29 12:26:39 -0500

    move `App.js` definition to `App.js` instead `index.js` to supress HMR warning / error

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,16 @@ import styles from './App.css';
 // import NavBar from './common/Navbar';
 // import NavLink from './common/NavLink';
 
-var navbar = {};
-navbar.brand = {linkTo: "/", text: "chrisrjones.com"};
-navbar.links = [
-    {linkTo: "/about", text: "About Me"},
-    {linkTo: "/contact", text: "Contact" },
-    {dropdown: true, text: "Contribute",  links: [
-        {linkTo: "signup", text: "Sign Up" },
-        {linkTo: "login", text: "Login" }
-    ]}
-];
+// var navbar = {};
+// navbar.brand = {linkTo: "/", text: "chrisrjones.com"};
+// navbar.links = [
+//     {linkTo: "/about", text: "About Me"},
+//     {linkTo: "/contact", text: "Contact" },
+//     {dropdown: true, text: "Contribute",  links: [
+//         {linkTo: "signup", text: "Sign Up" },
+//         {linkTo: "login", text: "Login" }
+//     ]}
+// ];
 
 // const NavBrand = (props) => (
 //   <Link to={props.linkTo}>
@@ -20,20 +20,25 @@ navbar.links = [
 //   </Link>
 // );
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className={styles.app}>
-        // <NavBar {...navbar}/>
-        <h2>Hello, React!</h2>
-        {this.props.children}
-      </div>
-      );
-    }
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className={styles.app}>
+//         // <NavBar {...navbar}/>
+//         <h2>Hello, React!</h2>
+//         {this.props.children}
+//       </div>
+//       );
+//     }
+// }
+
+// Create a new component.  This component should produce some HTML.
+const App = () => {
+  return <div>Hi Diana!</div>;
 }
 
 // App.propTypes = {
 // 	children: PropTypes.object.isRequired
 // };
 
-export default { App };
+export default App;
--- src/index.js
+++ src/index.js
@@ -26,11 +26,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App.js';
 
-// Create a new component.  This component should produce some HTML.
-const App = () => {
-  return <div>Hi!</div>;
-}
+
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.getElementById('root'));
