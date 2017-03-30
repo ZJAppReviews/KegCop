c0fe70d1d2019a5f201a94ef536b742f7360e63e
ipatch <chris.r.jones.1983@gmail.com>
2017-03-30 17:48:53 -0500

    add reference to 2 youtube embed links

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import NavBar from './common/NavBar-test';
 var navbar = {};
 navbar.brand =  {linkTo: "#", text: "KegCop"};
 navbar.links = [
-  {linkTo: "#", text: "Link 1"},
-  {linkTo: "#", text: "Link 2"}
+  {linkTo: "#", text: "Demonstration"},
+  {linkTo: "#", text: "Demonstration #2"}
 ];
 
 // Create a new component.  This component should produce some HTML.
@@ -13,7 +13,24 @@ const App = (props) => {
   return (
     <div id="parent">
       <NavBar {...navbar} />
-      <div id="diana">Hello, App!</div>
+      {/* see scratchpad.txt for removed div. */}
+      <div id="wilson">
+        <div className="embed-responsive embed-responsive-4by3">
+          <iframe className="embed-responsive-item"
+                src="https://www.youtube.com/embed/1a6hxUb3zfU"
+                frameBorder="0" allowFullScreen>
+          </iframe>
+        </div>
+      </div> {/* wilson */}
+      {/* insert video #2 below */}
+      <div id="chris">
+        <div className="embed-responsive embed-responsive-4by3">
+          <iframe className="embed-responsive-item"
+                src="https://www.youtube.com/embed/tNWhGGUwZjg"
+                frameBorder="0" allowFullScreen>
+          </iframe>
+        </div>
+      </div> {/* chris */}
     </div>
   );
 };
