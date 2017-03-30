007e31acc63634470ecde17d95e755340ba38f7c
ipatch <chris.r.jones.1983@gmail.com>
2017-03-30 12:19:45 -0500

    favicon is now showing in dev

--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,18 @@ module.exports = {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      },
+      // the below webpack config was sourced from this,
+      // https://github.com/coryhouse/react-slingshot/issues/128
+      // in order to load favicon.
+
+      {
+          test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+          loader: 'file-loader?name=[name].[ext]'  // <-- retain original file name
+      }
     ],
   },
 
