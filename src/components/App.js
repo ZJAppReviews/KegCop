50763284ed8706c99cf615616fea019a2a47abac
ipatch <chris.r.jones.1983@gmail.com>
2017-03-31 12:14:46 -0500

    remove erronous import statement in `App.js` and add import statement for ReactGA

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, {PropTypes} from 'react';
 import NavBar from './common/NavBar-test';
-// import iPhone from '../images/iphone-template.jpg';
 import iPhone from '../images/iphone-template.png';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-43223650-1');
 
 var navbar = {};
 navbar.brand =  {linkTo: "#", text: "KegCop"};
