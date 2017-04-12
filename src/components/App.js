5ea8bddb11e6febf9f3a42f2ecd1a278aabe20f0
ipatch <chris.r.jones.1983@gmail.com>
2017-04-11 19:51:35 -0500

    add 2 more nav links in the navbar

--- src/components/App.js
+++ src/components/App.js
@@ -3,65 +3,74 @@ import NavBar from './common/NavBar-test';
 import iPhone from '../images/iphone-template.png';
 import $ from 'jquery';
 
+const githubSource = "https://github.com/ipatch/KegCop"
+
+
+
 var navbar = {};
 navbar.brand =  {linkTo: "#", text: "KegCop"};
 navbar.links = [
+
   {linkTo: "#Demonstration", text: "Demonstration"},
-  {linkTo: "#Demonstration2", text: "Demonstration #2"}
+  {linkTo: "#Demonstration2", text: "Demonstration #2"},
+  {linkTo: <a href="https://github.com/ipatch/KegCop"></a>, text: "GitHub Source Code"},
+  {linkTo: "The Future of KegCop", text: "The Future of KegCop"}
 ];
 
 // Create a new component.  This component should produce some HTML.
 const App = (props) => {
-  return (
-    <div id="parent">
-      <NavBar {...navbar} id="navbar" />
-      {/* see scratchpad.txt for removed div. */}
-      <div id="container">
+  // render(){
+  //   const githubSource = "https://github.com/ipatch/KegCop"
+      return (
+        <div id="parent">
+          <NavBar {...navbar} id="navbar" />
+          {/* see scratchpad.txt for removed div. */}
+          <div id="container">
 
-        <div className="box" id="bluebox">
-            <div id="iphone">
-              <img className="iphone-template" src={iPhone} />
+            <div className="box" id="bluebox">
+                <div id="iphone">
+                  <img className="iphone-template" src={iPhone} />
+                </div>
             </div>
-        </div>
 
-          <div className="box" id="redbox">
-            <div id="message">
-              <h1>An iOS app for your kegerator!</h1>
-              <h4>KegCop is an open source iOS application that monitors a kegerator
-           by having drinkers create user accounts associated with the kegerator.
-           The admin of the kegerator will login with the root account,
-          and then issue credits to the users of the kegerator.</h4>
-            </div>
+              <div className="box" id="redbox">
+                <div id="message">
+                  <h1>An iOS app for your kegerator!</h1>
+                  <h4>KegCop is an open source iOS application that monitors a kegerator
+               by having drinkers create user accounts associated with the kegerator.
+               The admin of the kegerator will login with the root account,
+              and then issue credits to the users of the kegerator.</h4>
+                </div>
+              </div>
           </div>
-      </div>
 
 
-      <div id="Demonstration">
-        <a href="#Demonstration" className="scroll"></a>
-        <div className="embed-responsive embed-responsive-4by3">
-          <iframe className="embed-responsive-item"
-                src="https://www.youtube.com/embed/1a6hxUb3zfU"
-                frameBorder="0" allowFullScreen>
-          </iframe>
+          <div id="Demonstration">
+            <a href="#Demonstration" className="scroll"></a>
+            <div className="embed-responsive embed-responsive-4by3">
+              <iframe className="embed-responsive-item"
+                    src="https://www.youtube.com/embed/1a6hxUb3zfU"
+                    frameBorder="0" allowFullScreen>
+              </iframe>
+            </div>
+          </div> {/* wilson */}
+
+          {/* insert video #2 below */}
+          <div id="Demonstration2">
+            <a href="#Demonstration2" className="scroll"></a>
+            <div className="embed-responsive embed-responsive-4by3">
+              <iframe className="embed-responsive-item"
+                    src="https://www.youtube.com/embed/tNWhGGUwZjg"
+                    frameBorder="0" allowFullScreen>
+              </iframe>
+            </div>
+          </div> {/* chris */}
+          <div id="footer">
+            <p>Coded with &#9825; by: Chris Jones &copy; 2017</p>
+          </div>
         </div>
-      </div> {/* wilson */}
-
-      {/* insert video #2 below */}
-      <div id="Demonstration2">
-        <a href="#Demonstration2" className="scroll"></a>
-        <div className="embed-responsive embed-responsive-4by3">
-          <iframe className="embed-responsive-item"
-                src="https://www.youtube.com/embed/tNWhGGUwZjg"
-                frameBorder="0" allowFullScreen>
-          </iframe>
-        </div>
-      </div> {/* chris */}
-      <div id="footer">
-        <p>Coded with &#9825; by: Chris Jones &copy; 2017</p>
-      </div>
-    </div>
-
-  );
+    );
+  // };
 };
 
 // handle links with @href started with '#' only
